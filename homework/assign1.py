684ee202387451b12ba43a91656493226a9743fd
LovingChester <chelsea000719@gmail.com>
2021-09-15 01:09:32 -0400

    8888888

--- homework/assign1.py
+++ homework/assign1.py
@@ -45,5 +45,49 @@ for i in range(D_var_inner_row):
     for j in range(D_var_inner_row):
         correlate_matrix[i,j] = D_var_inner[i,j] / math.sqrt(D_var_inner[i,i]*D_var_inner[j,j])
 
-print(correlate_matrix)
+print('This is the correlation_matrix: {}'.format(correlate_matrix))
+
+# Convert the correlation matrix to the degree
+correlate_matrix_degree = np.degrees(np.arccos(correlate_matrix))
 print(np.degrees(np.arccos(correlate_matrix)))
+
+most_correlated = 99999
+most_correlated_coord = None
+least_correlated = -99999
+least_correlated_coord = None
+anti_correlated = -99999
+anti_correlated_coord = None
+for i in range(D_var_inner_row):
+    for j in range(D_var_inner_row):
+        if correlate_matrix_degree[i, j] < most_correlated and correlate_matrix_degree[i, j] != 0:
+            most_correlated = correlate_matrix_degree[i, j]
+            most_correlated_coord = (i, j)
+        if correlate_matrix_degree[i, j] > least_correlated and correlate_matrix_degree[i, j] <= 90:
+            least_correlated = correlate_matrix_degree[i, j]
+            least_correlated_coord = (i, j)
+        if correlate_matrix_degree[i, j] > anti_correlated:
+            anti_correlated = correlate_matrix_degree[i, j]
+            anti_correlated_coord = (i, j)
+
+print(most_correlated_coord, least_correlated_coord, anti_correlated_coord)
+
+max_chart_size = max(np.max(D[:, most_correlated_coord[0]]),
+                 np.max(D[:, most_correlated_coord[1]]))
+print(max_chart_size)
+min_chart_size = min(np.min(D[:, most_correlated_coord[0]]),
+                     np.min(D[:, most_correlated_coord[1]]))
+
+# plt.axis([int(min_chart_size), math.ceil(max_chart_size),
+#          int(min_chart_size), math.ceil(max_chart_size)])
+plt.plot(D[:, most_correlated_coord[0]], D[:, most_correlated_coord[1]], 'bx')
+plt.show()
+
+plt.plot(D[:, least_correlated_coord[0]], D[:, least_correlated_coord[1]], 'bx')
+plt.show()
+
+plt.plot(D[:, anti_correlated_coord[0]],
+         D[:, anti_correlated_coord[1]], 'bx')
+plt.show()
+
+
+print(anti_correlated)

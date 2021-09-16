64431c23995f175167ca5882b9d8cad83a83513c
LovingChester <chelsea000719@gmail.com>
2021-09-15 20:46:40 -0400

    jljl

--- homework/assign1.py
+++ homework/assign1.py
@@ -1,8 +1,11 @@
+from numpy.core.fromnumeric import size
 import pandas as pd
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+
+EPS = 0.0001
 
 D = pd.read_csv('energydata_complete.csv')
 D.pop('date')
@@ -12,40 +15,45 @@ print(D)
 # Convert to numpy datatype
 D = D.to_numpy()
 row = np.size(D, 0)
+col = np.size(D, 1)
+
+np.set_printoptions(precision=3)
 
 # Compute mean for each attribute
 mean = np.matmul(np.transpose(D), np.ones((row, 1))) / row
-print("This is the mean vector μ:\n{}".format(mean))
+print("This is the mean vector μ:\n{}\n".format(mean))
 
 # Compute total variance
 var_D = sum(sum(D*D)) / row - np.linalg.norm(mean) ** 2
-print("This is the total variance: {}".format(var_D))
+print("This is the total variance:\n{:.3f}\n".format(var_D))
 
 # Compute the center data matrix
 D_center = D - np.matmul(np.ones((row, 1)), np.transpose(mean))
 
 # Compute the sample covariance inner product form
 D_var_inner = np.matmul(np.transpose(D_center), D_center) / row
-print("This is the sample convariance in inner product form{}".format(D_var_inner))
+print("This is the sample convariance in inner product form:\n{}\n".format(D_var_inner))
 
+#D_var_inner_row = np.size(D_var_inner,0)
 # Compute the sample covariance outer product form
 #print(np.outer(np.array([[1],[2],[3],[4]]), np.array([1,2,3])))
-# D_var_outer = np.zeros((row, row))
-# D_center_T = np.transpose(D_center)
-# for i in range(row):
-#     D_var_outer = D_var_outer + np.outer(D_center[ :,i], D_center_T[i, :])
-# print(D_var_outer)
+D_var_outer = np.zeros((col, col))
+#D_center_T = np.transpose(D_center)
+for i in range(row):
+    D_var_outer = D_var_outer + np.outer(D_center[i, :].reshape(col,1), D_center[i, :])
+D_var_outer = D_var_outer / row
+print("This is the sample convariance in outer product form:\n{}\n".format(D_var_outer))
 
 # Compute the correlation matrix
-print(np.size(D_var_inner,1))
-D_var_inner_row = np.size(D_var_inner,0)
-correlate_matrix = np.zeros((D_var_inner_row, D_var_inner_row))
+#print(np.size(D_var_inner,1))
+#D_var_inner_row = np.size(D_var_inner,0)
+correlate_matrix = np.zeros((col, col))
 #print(correlate_matrix)
-for i in range(D_var_inner_row):
-    for j in range(D_var_inner_row):
+for i in range(col):
+    for j in range(col):
         correlate_matrix[i,j] = D_var_inner[i,j] / math.sqrt(D_var_inner[i,i]*D_var_inner[j,j])
 
-print('This is the correlation_matrix: {}'.format(correlate_matrix))
+print('This is the correlation_matrix:\n{}\n'.format(correlate_matrix))
 
 # Convert the correlation matrix to the degree
 correlate_matrix_degree = np.degrees(np.arccos(correlate_matrix))
@@ -57,8 +65,8 @@ least_correlated = -99999
 least_correlated_coord = None
 anti_correlated = -99999
 anti_correlated_coord = None
-for i in range(D_var_inner_row):
-    for j in range(D_var_inner_row):
+for i in range(col):
+    for j in range(col):
         if correlate_matrix_degree[i, j] < most_correlated and correlate_matrix_degree[i, j] != 0:
             most_correlated = correlate_matrix_degree[i, j]
             most_correlated_coord = (i, j)
@@ -73,21 +81,44 @@ print(most_correlated_coord, least_correlated_coord, anti_correlated_coord)
 
 max_chart_size = max(np.max(D[:, most_correlated_coord[0]]),
                  np.max(D[:, most_correlated_coord[1]]))
-print(max_chart_size)
+# print(max_chart_size)
 min_chart_size = min(np.min(D[:, most_correlated_coord[0]]),
                      np.min(D[:, most_correlated_coord[1]]))
 
 # plt.axis([int(min_chart_size), math.ceil(max_chart_size),
 #          int(min_chart_size), math.ceil(max_chart_size)])
+plt.title("most correlated")
+plt.xlabel('A ' + str(most_correlated_coord[0]+1))
+plt.ylabel('A ' + str(most_correlated_coord[1]+1))
 plt.plot(D[:, most_correlated_coord[0]], D[:, most_correlated_coord[1]], 'bx')
 plt.show()
 
+plt.title("least correlated")
+plt.xlabel('A ' + str(least_correlated_coord[0]+1))
+plt.ylabel('A ' + str(least_correlated_coord[1]+1))
 plt.plot(D[:, least_correlated_coord[0]], D[:, least_correlated_coord[1]], 'bx')
 plt.show()
 
+plt.title("anti correlated")
+plt.xlabel('A ' + str(anti_correlated_coord[0]+1))
+plt.ylabel('A ' + str(anti_correlated_coord[1]+1))
 plt.plot(D[:, anti_correlated_coord[0]],
          D[:, anti_correlated_coord[1]], 'bx')
 plt.show()
 
+# print(anti_correlated)
+np.random.seed(13)
+x_init = np.random.normal(size=(col, 1))
+x_new = None
+while(True):
+    x_new = np.matmul(D_var_inner,x_init)
+    #print(x_new)
+    x_scaled = x_new / np.max(np.abs(x_new))
+    if np.linalg.norm(x_scaled - x_init) < EPS:
+        break
+    #print(x_init)
+    x_init = x_scaled
 
-print(anti_correlated)
+print(x_new)
+x_normalize = x_new / np.linalg.norm(x_new)
+print(x_normalize)

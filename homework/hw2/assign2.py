892931551af9149f07986a2b1f6ec6e6f36a0629
LovingChester <chelsea000719@gmail.com>
2021-09-21 18:19:31 -0400

    jj

--- homework/hw2/assign2.py
+++ homework/hw2/assign2.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+alpha = 0.975
+
 D = pd.read_csv('energydata_complete.csv')
 D.pop('date')
 D.pop('rv2')
@@ -32,6 +34,34 @@ D_var_inner = np.matmul(np.transpose(D_center), D_center) / row
 
 e_values, e_vectors = np.linalg.eigh(D_var_inner)
 
+print(e_values[col:])
+
 print(e_vectors)
+
+# Compute PCA
+r = 1
+while(True):
+    # frac = 0
+    # for i in range(r):
+    #     frac = frac + e_values[i]
+    frac = sum(e_values[col-r:]) / sum(e_values)
+    if r <= 3:
+        MSE = sum(e_values) - sum(e_values[col-r:])
+        print("MSE is {:.3f}".format(MSE))
+    if frac >= alpha:
+        break
+    r += 1
+
+print("Dimensions required: {}".format(r))
+
+# First PC
+proj_u1 = np.matmul(D_center, e_vectors[:, [col-1, col-1]])
+plt.plot(proj_u1, np.array(row*[0]), 'bx')
+plt.show()
+
+# Second PC
+proj_u2 = np.matmul(D_center, e_vectors[:, [col-1, col-2]])
+plt.plot(proj_u2[:, [0,0]], proj_u2[:, [1,1]], 'bx')
+plt.show()
 
 

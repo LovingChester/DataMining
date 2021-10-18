85002ea10218cad21669361c1b7d36c0fe1bde32
LovingChester <chelsea000719@gmail.com>
2021-10-18 00:47:55 -0400

    assign 4

--- homework/hw4/Assign4.py
+++ homework/hw4/Assign4.py
@@ -7,15 +7,14 @@ from sklearn.preprocessing import StandardScaler
 
 np.set_printoptions(precision=3, suppress=False, threshold=5)
 
-ALPHA = 0.1
-ETA = 0.1
-EPS = 0.001
+ALPHA = 10
+ETA = 0.00001
+EPS = 0.0001
 MAXITER = 5000
 
 D = pd.read_csv('energydata_complete.csv')
 D.pop('date')
 D.pop('rv2')
-print(D)
 
 D = D.to_numpy()
 # scale the data
@@ -28,11 +27,30 @@ D_valid = D[range(13735, 15735), :]
 D_test = D[range(15735, 19735), :]
 
 # selecting the response and independent variables
-Dx_train = D_train[:, range(1, 28)]
+Dx_train = D_train[:, range(1, 27)]
 Dy_train = D_train[:, [0]]
-Dx_valid = D_valid[:, range(1, 28)]
+Dx_valid = D_valid[:, range(1, 27)]
 Dy_valid = D_valid[:, [0]]
-Dx_test = D_test[:, range(1, 28)]
+Dx_test = D_test[:, range(1, 27)]
 Dy_test = D_test[:, [0]]
 
+# start linear regression
+Dx_train = np.insert(Dx_train, 0, 13735*[1], axis=1)
+t = 0
+w = np.ones((27, 1))
+# print(np.average(Dx_train[:, 0]), np.var(Dx_train[:, 0]))
+while(t < MAXITER):
+    gradient = -np.matmul(np.transpose(Dx_train), Dy_train) + \
+        np.matmul(np.matmul(np.transpose(Dx_train), Dx_train), w) + ALPHA * w
+    w_prev = np.copy(w)
+    #print(gradient)
+    w = w - ETA * gradient
+    if np.linalg.norm(w - w_prev) <= EPS:
+        break
+    t += 1
+
+print(w)
+print(t)
+
+# start validation
 

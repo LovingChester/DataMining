608bc844027a2342f48cd39de9e71e562a33333a
LovingChester <chelsea000719@gmail.com>
2021-10-24 01:48:57 -0400

    assign5

--- homework/hw5/Assign5.py
+++ homework/hw5/Assign5.py
@@ -3,6 +3,8 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import random as rd
+import time
 
 np.set_printoptions(precision=3, suppress=False, threshold=5)
 
@@ -21,7 +23,12 @@ def SVM_DUAL_LINEAR(Dx, Dy):
     for i in range(row):
         K_row = []
         for j in range(row):
-            K_row.append(np.dot(Dx[i, :], Dx[j, :]) ** 2)
+            if KERNEL == "linear":
+                K_row.append(np.dot(Dx[i, :], Dx[j, :]) ** 2)
+            else:
+                diff = Dx[i, :] - Dx[j, :]
+                kernel = math.e ** (-np.dot(diff, diff) / (2 * KERNEL_PARAM))
+                K_row.append(kernel)
         K.append(K_row)
     
     K = np.array(K)
@@ -38,11 +45,23 @@ def SVM_DUAL_LINEAR(Dx, Dy):
     alpha = np.zeros((row, 1))
     while t < MAXITER:
         alpha_prev = np.copy(alpha)
-        for k in range(row):
+        l = list(range(row))
+        rd.shuffle(l)
+        for k in l:
+            alpha[k, 0] = alpha[k, 0] + step_size[k] * (1 - \
+                Dy[k, 0] * np.sum(alpha * Dy * K[:, [k]]))
             
-            return
+            if alpha[k, 0] < 0:
+                alpha[k, 0] = 0
+            if alpha[k, 0] > C:
+                alpha[k, 0] = C
+        
+        if np.linalg.norm(alpha - alpha_prev) < EPS:
+            break
 
-    return
+        t = t + 1
+
+    return alpha
 
 D = pd.read_csv(FILENAME)
 D.pop('date')
@@ -58,9 +77,31 @@ D_test = D[range(7000, 12000), :]
 # selecting the response and independent variables
 Dx_train = D_train[:, range(1, 27)]
 Dy_train = D_train[:, [0]]
+for i in range(5000):
+    if Dy_train[i, 0] <= 50:
+        Dy_train[i, 0] = 1
+    else:
+        Dy_train[i, 0] = -1
+
 Dx_valid = D_valid[:, range(1, 27)]
 Dy_valid = D_valid[:, [0]]
+for i in range(2000):
+    if Dy_valid[i, 0] <= 50:
+        Dy_valid[i, 0] = 1
+    else:
+        Dy_valid[i, 0] = -1
+
 Dx_test = D_test[:, range(1, 27)]
 Dy_test = D_test[:, [0]]
+for i in range(5000):
+    if Dy_test[i, 0] <= 50:
+        Dy_test[i, 0] = 1
+    else:
+        Dy_test[i, 0] = -1
 
-#SVM_DUAL_LINEAR(Dx_train, Dy_train)
+start = time.time()
+
+alpha = SVM_DUAL_LINEAR(Dx_train, Dy_train)
+
+end = time.time()
+print(end - start)
